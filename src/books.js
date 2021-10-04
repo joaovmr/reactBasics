68945f3e0847b5fcce58a7fc19792337423cf70a
joaovmr <48189739+joaovmr@users.noreply.github.com>
2021-10-04 11:19:06 -0300

    Modularizing

---
+++ src/books.js
@@ -0,0 +1,23 @@
+//Setup variables
+export const books = [ // Named export
+    {
+      id:1,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+      title:'I Love You to the Moon and Back',
+      author: 'Amelia Hepworth'
+    },
+  
+    {
+      id:2,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81MgsNre3IL._AC_UL200_SR200,200_.jpg',
+      title:'The Wish',
+      author: 'Nicholas Sparks'
+    },
+  
+    {
+      id:3,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81Ta5Jce%2BeL._AC_UL200_SR200,200_.jpg',
+      title:'Cloud Cuckoo Land: A Novel',
+      author: 'Anthony Doerr'
+    },
+  ]
--- src/index.js
+++ src/index.js
@@ -4,29 +4,9 @@ import ReactDom from "react-dom";
 //CSS
 import './index.css'; 
 
-//Setup variables
-const books = [
-  {
-    id:1,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
-    title:'I Love You to the Moon and Back',
-    author: 'Amelia Hepworth'
-  },
-
-  {
-    id:2,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81MgsNre3IL._AC_UL200_SR200,200_.jpg',
-    title:'The Wish',
-    author: 'Nicholas Sparks'
-  },
-
-  {
-    id:3,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81Ta5Jce%2BeL._AC_UL200_SR200,200_.jpg',
-    title:'Cloud Cuckoo Land: A Novel',
-    author: 'Anthony Doerr'
-  },
-]
+//Imported Files
+import { books } from "./books"
+import Book from "./Book";
 
 function BookList(){ //React Component
   return (
@@ -40,32 +20,6 @@ function BookList(){ //React Component
       })}
     </section>
   );
-}
-
-const Book = ({img,title,author}) => {   
-  const clickHandler = (e) => {
-    console.log(e)
-    console.log(e.target)
-    alert('hello world');
-  }
-
-  const complexExample = (author) => {
-    console.log(author);
-  }
-
-  return (
-    <article className = 'book' onMouseOver = {() => {
-      console.log(title);
-    }}>
-      <img src = {img} alt = ""/>
-      <h1 onClick = {() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type = "button" onClick = {clickHandler}>
-        Reference Example
-      </button>
-      <button type = "button" onClick = {() => complexExample(author)}> More complex example</button>
-    </article>
-  )
 }
 
 
