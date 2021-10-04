1011936b821e1f9bd8896d3735120139c22544c4
joaovmr <48189739+joaovmr@users.noreply.github.com>
2021-10-04 11:06:19 -0300

    Adding events and cleaning the code

--- src/index.js
+++ src/index.js
@@ -1,50 +1,73 @@
 import React from "react"; //ES6 modules
 import ReactDom from "react-dom";
+
+//CSS
 import './index.css'; 
+
+//Setup variables
+const books = [
+  {
+    id:1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title:'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth'
+  },
+
+  {
+    id:2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81MgsNre3IL._AC_UL200_SR200,200_.jpg',
+    title:'The Wish',
+    author: 'Nicholas Sparks'
+  },
+
+  {
+    id:3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Ta5Jce%2BeL._AC_UL200_SR200,200_.jpg',
+    title:'Cloud Cuckoo Land: A Novel',
+    author: 'Anthony Doerr'
+  },
+]
 
 function BookList(){ //React Component
   return (
     <section className = 'booklist'>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
+      {books.map((book) => {
+        return (
+          <Book key = {book.id} {...book}></Book> //...book pass 
+          //all the properties of the object instead of the object 
+          //itself this is the spread operator
+        );
+      })}
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({img,title,author}) => {   
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert('hello world');
+  }
+
+  const complexExample = (author) => {
+    console.log(author);
+  }
+
   return (
-    <article className = 'book'>
-      <Image/>
-      <Title/>
-      <Author/>
+    <article className = 'book' onMouseOver = {() => {
+      console.log(title);
+    }}>
+      <img src = {img} alt = ""/>
+      <h1 onClick = {() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type = "button" onClick = {clickHandler}>
+        Reference Example
+      </button>
+      <button type = "button" onClick = {() => complexExample(author)}> More complex example</button>
     </article>
   )
 }
 
-const Image = () => {
-  return (
-    <img src = "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-    alt = ""/>
-  )
-};
-
-const Title = () => {
-  return (
-    <h1>I Love You to the Moon and Back</h1>
-  )
-}
-
-const Author = () => {
-  return (
-    <h4 style={{color:'#617d98',fontSize:'0,75rem',marginTop:'0.25rem'}}>Amelia Hepworth</h4>
-  )
-};
 
 ReactDom.render(<BookList/>,document.getElementById('root')
 ); // <BookList/> == <BookList></BookList>
